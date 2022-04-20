d54c4caae60ba0a2a9b1646586625919559e879e
Attmoon <gus89580@naver.com>
2022-04-20 16:47:09 +0900

    logIn using filter

--- src/chap19/servlet5/S5Servlet03.java
+++ src/chap19/servlet5/S5Servlet03.java
@@ -1,23 +1,25 @@
 package chap19.servlet5;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class S5Servlet02
+ * Servlet implementation class S5Servlet03
  */
-@WebServlet("/s5/servlet02")
-public class S5Servlet02 extends HttpServlet {
+@WebServlet("/s5/servlet03")
+public class S5Servlet03 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public S5Servlet02() {
+    public S5Servlet03() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,21 +28,30 @@ public class S5Servlet02 extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		String logout = request.getParameter("logout");
+		
+		if (logout != null && logout.equals("true")) {
+			request.getSession().invalidate();
+		}
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
 		
-		String nickName = request.getParameter("nickName");
-		String song = request.getParameter("song");
+		if (id != null && id.equals(pw)) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loggedInUser", id);
+		}
 		
-		System.out.println(nickName);
-		System.out.println(song);
-	
+		String location = request.getContextPath() + "/chap19/ex02.jsp";
+		response.sendRedirect(location);
 	}
 
 }
