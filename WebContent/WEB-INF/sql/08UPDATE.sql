94efcfaded1a8ceee05b76146537acbffba6bc14
Attmoon <99157555+Attmoon@users.noreply.github.com>
2022-04-25 17:39:51 +0900

    update

--- WebContent/WEB-INF/sql/08UPDATE.sql
+++ WebContent/WEB-INF/sql/08UPDATE.sql
@@ -1,8 +1,34 @@
 USE w3schools;
 
+-- safe mode 해제
+SET SQL_SAFE_UPDATES = 0;
+
 SELECT * FROM Employees ORDER BY EmployeeID DESC;
 DESC Employees;
 UPDATE Employees
 SET LastName = 'Sunja',
 	FirstName = 'Yuhjung'
 WHERE EmployeeID = 1;
+
+-- 예제1) 1번 고객(Customers table)의 
+-- 계약자명(ContactName)은 'Alfred Schmidt'
+-- 도시(City)는 'Frankfurt'로 변경
+SELECT * FROM Customers ORDER BY CustomerID DESC;
+DESC Customers;
+UPDATE Customers
+SET ContactName = 'Alfred Schmidt',
+	City = 'Frankfurt'
+WHERE CustomerID = 1;
+
+-- 한번에 여러 record를 변경하는게 위험한 코드여서 
+UPDATE Customers
+SET ContactName='Dude'
+WHERE Country='Mexico';
+
+SELECT * FROM Customers WHERE Country = 'Mexico';
+
+SELECT * FROM Products;
+UPDATE Products
+SET Price = Price + 2;
+
+SELECT * FROM Employees;
